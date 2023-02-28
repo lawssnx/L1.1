7d31a7b48efe81af68353e0b447022046fc03adb
anastasi <120751391+lawssnx@users.noreply.github.com>
2023-02-28 21:08:56 +0200

    Update L11.cs

--- L11.cs
+++ L11.cs
@@ -1,8 +1,40 @@
-﻿using System;
-
-public class Class1
+using System;
+using System.Collections.Generic;
+class Programm
 {
-	public Class1()
-	{
-	}
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть список чисел через пробіл:");
+        List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        Console.Write("Введіть індекс елемента, який потрібно видалити:");
+        int index = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("_______");
+
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("_______");
+
+        numbers.RemoveAt(index);
+
+        int newIndex = index;
+        while (newIndex < numbers.Count && numbers[newIndex] < numbers[index])
+        {
+            newIndex++;
+            numbers.Insert(newIndex, numbers[index]);
+
+        }
+
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+
+        Console.ReadKey();
+
+
+    }
 }
